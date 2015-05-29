0a2850b1612c28e9baa0c39b5b15819303e3ceb1
ian schoonover <iantskon@gmail.com>
2015-05-29 11:29:27 +0200

    Add random group

--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -1,23 +1,26 @@
 class ClassroomsController < ApplicationController
-  	STUDENTS = [
-  		"Bailey","Austin","Blake","Yunny","Olivia","Alex",
-  		"Charlotte","Asad","Han","Kate","Oscar","Sophie","Zack","Matthew",
-  		"Gloryah","Jack","Enrique","Ben","Stanley","Kaan","Dilys","Molly",
-  		"Susana","Emily","Pavi","Daniel","Arya","Urvish"
-  	]
+	STUDENTS = [
+		"Bailey","Austin","Blake","Yunny","Olivia","Alex",
+		"Charlotte","Asad","Han","Kate","Oscar","Sophie","Zack","Matthew",
+		"Gloryah","Jack","Enrique","Ben","Stanley","Kaan","Dilys","Molly",
+		"Susana","Emily","Pavi","Daniel","Arya","Urvish"
+	]
 
   def index
   	@welcome_message = "Welcome to my classroom app!"
   end
 
   def students
-
   	@students = STUDENTS.sort
   end
 
   def random_student
-  	@students = STUDENTS
-  	while (@random_student = @students.sample) == params[:student]
-  	end
+    @students = STUDENTS
+    while (@random_student = @students.sample) == params[:student]
+    end
+  end
+
+  def random_group
+    @groups = STUDENTS.shuffle.each_slice(4).to_a
   end
 end
